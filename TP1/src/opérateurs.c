3e221a52fb283defd97a2c49e5c21489970637e6
rayan92110 <rayanhassani37@gmail.com>
2025-11-21 11:43:58 +0100

    Update opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -8,11 +8,12 @@ int b = 4;
     printf("a+b = %d\n", a+b);
     printf("a*b = %d\n", a*b);
     printf("a/b = %d\n", a/b);
-    printf("a modulo b = %d\n", a%%b);
+    printf("a modulo b = %d\n", a%b);
     printf("a=b esce vrai ? : %d\n", a=b);
     printf("a superieur b esce vrai ? : %d\n", a>b);
    
 }
+
 
 
 
