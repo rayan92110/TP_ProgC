0fa64df794fe4e3b31e8fcfef8a8d27aed229db3
rayan92110 <rayanhassani37@gmail.com>
2025-11-21 10:30:32 +0100

    Update opérateurs.c

---
+++ TP1/src/opérateurs.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+int a = 16;
+int b = 4;  
+    printf("a-b = %f", a-b);
+    printf("a+b = %f", a+b);
+    printf("a*b = %f", a*b);
+    printf("a/b = %f", a/b);
+   
+}
