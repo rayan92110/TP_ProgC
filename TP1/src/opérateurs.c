41ad52d8c1846e92e2e5058f01a3b2c8bc5deebe
rayan92110 <rayanhassani37@gmail.com>
2025-11-21 11:47:06 +0100

    Update opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 int main() {
 int a = 16;
@@ -9,10 +10,13 @@ int b = 4;
     printf("a*b = %d\n", a*b);
     printf("a/b = %d\n", a/b);
     printf("a modulo b = %d\n", a%b);
-    printf("a=b esce vrai ? : %d\n", a=b);
-    printf("a superieur b esce vrai ? : %d\n", a>b);
+    bool egal (a==b);
+    bool sup (a>b);
+    printf("a=b esce vrai ? : %d\n", egal);
+    printf("a superieur b esce vrai ? : %d", sup);
    
 }
+
 
 
 
