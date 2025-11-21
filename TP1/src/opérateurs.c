d17f5561eab8bd46c62b30b9457519d92ad9da38
rayan92110 <rayanhassani37@gmail.com>
2025-11-21 11:55:01 +0100

    Update opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -12,10 +12,19 @@ int b = 4;
     printf("a modulo b = %d\n", a%b);
     bool egal = (a==b);
     bool sup = (a>b);
+    if (egal ==0 || sup ==0){
+        egal = "false "
+        sup = "false "
+    }
+    else ( egal == 1 || sup == 1){
+        egal == " true"
+        sup == " true"
+    }
     printf("a=b esce vrai ? : %d\n", egal);
     printf("a superieur b esce vrai ? : %d", sup);
    
 }
+
 
 
 
