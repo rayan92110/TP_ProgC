dc5f15e352667613695aba3281f3f093fa67ca50
rayan92110 <rayanhassani37@gmail.com>
2025-12-01 15:44:18 +0100

    Update pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n = 5;
+    int i, j;
+
+    for (i = 1; i <= n; i++) {
+
+        // Espaces pour centrer
+        for (j = 1; j <= n - i; j++)
+            printf(" ");
+
+        // Nombres croissants
+        for (j = 1; j <= i; j++)
+            printf("%d", j);
+
+        // Nombres décroissants
+        for (j = i - 1; j >= 1; j--)
+            printf("%d", j);
+
+        printf("\n");
+    }
+
+    printf("Pyramide générée.\n");
+
+    return 0;
+}
 
